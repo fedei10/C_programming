832cfe0667fc1dae636fb815b070ee9229b2e629
fedi bellakhel <114336963+fedei10@users.noreply.github.com>
2022-10-07 11:26:49 +0100

    Add files via upload

---
+++ C programming/solo/f1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+void main () {
+ int a , b ; 
+printf("give two integers : ");
+ scanf("%d%d",&a,&b);
+printf("the square of %d is %f \n ", a,pow(a,2));
+printf ("\n %d + %d = %d\n", a, b, a+b) ;
+printf ("La racine carré de %d est %f\n", a, sqrt(a)) ;
+
+ }
