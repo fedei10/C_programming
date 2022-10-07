7094bc11abe865e812bc5c8d040accfee15c0e45
fedi bellakhel <114336963+fedei10@users.noreply.github.com>
2022-10-07 11:07:58 +0100

    Add files via upload

---
+++ C programming/solo/f0.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main ()
+ {float a = 5.7,b = 9.34;
+ printf("\n\n\n**** this is my first C programme ****\n");
+ printf("\tLa somme de %f et %f = %f\n",a ,b , a+b);
+ printf("\t%f * %f = %f\n",a ,b, a*b);
+ printf("\ta=%f , b=%f , a - b = %f",a ,b ,a-b);
+ } 
+
+
